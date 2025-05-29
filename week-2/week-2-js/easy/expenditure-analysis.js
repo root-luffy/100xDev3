0347fbb4a34ba5dfd1c2a4b61e1bff90473253f6
Luffy <root.luffy5@gmail.com>
2025-05-29 23:53:06 +0530

    wip

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -12,9 +12,57 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
+let input = [{
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	},
+  {
+		id: 2,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Vadaa',
+	},
+  {
+		id: 3,
+		timestamp: 1656076800003,
+		price: 100,
+		category: 'Vehicle',
+		itemName: 'Ferrari',
+	},{
+		id: 4,
+		timestamp: 1656076800003,
+		price: 1400,
+		category: 'Bacteria',
+		itemName: 'Ketoo',
+	}
+]
+
+
 
 function calculateTotalSpentByCategory(transactions) {
+let x = loopover(transactions);
+const myMap = new Map();
+myMap.set(transactions[0].category,)
   return [];
 }
+
+
+
+
+
+
+function loopover(transactions)
+{
+for(let i=0;i<transactions.length;i++)
+{
+console.log(transactions[i].category,transactions[i].price);
+}
+}
+
+calculateTotalSpentByCategory(input);
 
 module.exports = calculateTotalSpentByCategory;
